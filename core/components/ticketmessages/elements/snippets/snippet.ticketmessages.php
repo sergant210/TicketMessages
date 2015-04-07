300950210c02d8503e7fdc3b7bcd7f253970e957
Sergey Shlokov <sergant210@bk.ru>
2015-04-07 10:26:45 +0400

    Update snippet.ticketmessages.php

--- core/components/ticketmessages/elements/snippets/snippet.ticketmessages.php
+++ core/components/ticketmessages/elements/snippets/snippet.ticketmessages.php
@@ -34,12 +34,14 @@ if ($modx->user->hasSessionContext('mgr') && !empty($showLog)) {
 }
 
 $msg_refresh_interval = $modx->getOption('tm.msg_refresh_interval', null, '20')*1000;
+$scriptProperties['resource'] = $modx->resource->id;
+$_SESSION['ticketmessages']['sp'] = $scriptProperties;
 //Регистрируем стили
 //Если Bootstrap уже грузится, то отключите загрузку
 $modx->regClientCSS($modx->getOption('assets_url').'components/ticketmessages/css/lib/bootstrap.min.css');
 $modx->regClientCSS($modx->getOption('assets_url').'components/ticketmessages/css/messages.css');
 //Регистрируем скрипты
-$modx->regClientStartupScript("<script type=\"text/javascript\">\n\tvar msg_refresh_interval=".$msg_refresh_interval.",\n\tresource=".$modx->resource->id.";\n</script>", true);
+$modx->regClientStartupScript("<script type=\"text/javascript\">\n\tvar msg_refresh_interval=".$msg_refresh_interval.";\n</script>", true);
 $modx->regClientScript($modx->getOption('assets_url').'components/ticketmessages/js/messages.js');
 
 // Return output
